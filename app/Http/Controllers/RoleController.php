f162a6fe6b5bca0ad2cafa64eaf29ae87ffcb686
kanyuu-k <itumo_rakuten@yahoo.co.jp>
2021-08-05 00:43:14 +0000

    asset_https

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -8,14 +8,17 @@ use App\Models\User;
 
 class RoleController extends Controller
 {
-    public function attach(Request $request, User $user) {
-        $roleId=request()->input('role');
+    public function attach(Request $request, User $user)
+    {
+        $roleId = request()->input('role');
         $user->roles()->attach($roleId);
         return back();
     }
 
-    public function detach(Request $request, User $user) {
-        $roleId=request()->input('role');
+    public function detach(Request $request, User $user)
+    {
+        $roleId = request()->input('role');
         $user->roles()->detach($roleId);
         return back();
     }
+}
